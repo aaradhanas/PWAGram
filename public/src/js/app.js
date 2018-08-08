40835622f16731dbbc032e912f66bedeed3264ce
Aaradhana Sridharan <aaradhana.sridharan@gmail.com>
2018-08-08 22:48:53 +0530

    Service worker lifecycle

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,6 @@
 // navigator is the browser. We check for the property 'serviceWorker' in navigator to ensure support.
-if ( 'serviceWorker' in navigator){
+if ('serviceWorker' in navigator){
+    // Registration of service worker happens on every page reload.
     navigator.serviceWorker
         .register('/sw.js', {scope: '/'})
         .then( function() {
--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,28 @@
+/*
+    Installation event is triggered by the browser if the service worker code is changed.
+*/
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker..', event);
 });
 
+/*
+    Activation event is triggered by the browser. It does not happen until all the opened tabs 
+    of the app are closed and reopened.
+*/
 self.addEventListener('activate', function(event) {
     console.log('[Service Worker] Activating Service Worker..', event);
     // This ensures the SW is activated correctly. Not really needed, but it might fail if not present.
-    // Might not be required in the future.
+    // Adding this makes the activation more robust..Might not be required in the future.
     return self.clients.claim();
+});
+
+/* 
+    Fetch event is trigged by the application while fetching CSS, JS, images or data.
+    Every fetch request of the app goes through the service worker and so does every response.
+    The service worker acts like a proxy.
+*/
+self.addEventListener('fetch', function(event) {
+    console.log('[Service Worker] Fetching something..', event);
+    // We can override the response using the below method
+    event.respondWith( fetch(event.request) );
 });
