b0d593b8ea8456d64548a336f0251bcd478cfb50
Aaradhana Sridharan <aaradhana.sridharan@gmail.com>
2018-08-23 07:06:49 +0530

    Precaching app shell

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,21 @@ self.addEventListener('install', function(event) {
         caches.open('static')
             .then(function(cache){
                 console.log('[Service Worker] Precaching App Shell..');
-                cache.add('/src/js/app.js');
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/material.min.js',
+                    '/src/js/polyfills/promise.js',
+                    '/src/js/polyfills/fetch.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/main-image.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                ]);
             })
     );
 });
@@ -33,5 +47,16 @@ self.addEventListener('activate', function(event) {
 */
 self.addEventListener('fetch', function(event) {
     // We can override the response using the below method
-    event.respondWith( fetch(event.request) );
+    event.respondWith(
+        // Looks at all the available sub caches and check if a given request is present   
+        caches.match(event.request)
+            .then(function(response){
+                // Response is null if the request is not present in the cache
+                if(response){
+                    return response;
+                } else{
+                    return fetch(event.request);
+                }
+            })
+    );
 });
